02c41f62508a45e7d6de3f3d56c3d68515368982
Damien Pobel <damien@pobel.fr>
2016-01-06 17:01:23 +0100

    Added the conversion script

---
+++ convert.js
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+
+var metalsmith = require('metalsmith');
+
+function getPublishedDate(filePath) {
+    var filename = filePath.replace('post/', ''),
+        tmp = filename.split('-');
+
+    return tmp[0] + '-' + tmp[1] + '-' + tmp[2];
+}
+
+function renameFile(filePath, files, done) {
+    var file = files[filePath],
+        publishedDate = getPublishedDate(filePath),
+        newPath = filePath.replace('post/' + publishedDate + '-', 'post/');
+
+    file.published = publishedDate;
+    delete files[filePath];
+    if ( files[newPath] ) {
+        done(new Error('Name collision on ' + newPath));
+    }
+    files[newPath] = file;
+}
+
+function renameFiles(files, metalsmith, done) {
+    Object.keys(files).forEach(function (filePath) {
+        if ( filePath.startsWith("post/20") ) {
+            renameFile(filePath, files, done);
+        }
+    });
+    done();
+}
+
+function keepMetadata(files, metalsmith, done) {
+    var excludeProps = ['contents', 'stats', 'mode'];
+
+    Object.keys(files).forEach(function (filePath) {
+        var file = files[filePath],
+            line = "---\n";
+
+        Object.keys(file).forEach(function (key) {
+            if ( excludeProps.indexOf(key) === -1 ) {
+                line += key + ": " + file[key].toString() + "\n";
+            }
+        });
+        line += "---\n";
+        file.contents = line + file.contents;
+    });
+    done();
+}
+
+metalsmith(__dirname)
+    .source('src')
+    .use(renameFiles)
+    .use(keepMetadata)
+    .destination('result')
+    .build(function (error, res) {
+        if ( error ) {
+            console.error("Build failed: " + error.message);
+            console.log(error.stack);
+            process.exit(1);
+        }
+        console.log('Build successful:');
+        Object.keys(res).forEach(function (key) {
+            console.log('- ' + key);
+        });
+    });
