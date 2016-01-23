9b061a0e13fb5bbbee3180cda37bd5bfd97903b8
Damien Pobel <damien@pobel.fr>
2016-01-23 18:54:55 +0100

    Post a comment on the pull request when the build is done

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,4 +6,6 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
     rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/web/
 else
     rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/testing/${TRAVIS_PULL_REQUEST}.damien.pobel.fr
+    COMMENT="Test it at http://${TRAVIS_PULL_REQUEST}.damien.pobel.fr/"
+    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/dpobel/damien.pobel.fr/issues/${TRAVIS_PULL_REQUEST}/comments" > /dev/null
 fi
