340ecf406f10b50dea9ef0a42a8728cf51431f89
Damien Pobel <damien@pobel.fr>
2018-01-11 13:53:03 +0100

    Fixed comment added on github to have an HTTPs link [ci skip]

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,6 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
     rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/web/
 else
     rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/testing/${TRAVIS_PULL_REQUEST}.damien.pobel.fr
-    COMMENT="Test it at http://${TRAVIS_PULL_REQUEST}.damien.pobel.fr/"
+    COMMENT="Test it at https://${TRAVIS_PULL_REQUEST}.damien.pobel.fr/"
     curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
