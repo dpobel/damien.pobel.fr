a1c9635d83f32dbf9b7473a2e475bc0a928414d8
Damien Pobel <damien@pobel.fr>
2018-03-04 22:37:51 +0100

    fix #86: bug in veille script [ci skip]

--- bin/cron-veille.sh
+++ bin/cron-veille.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 LAST_DATE_FILE=".veille-date"
-LAST_DATE=`cat $LAST_DATE_FILE`
 POST_FILENAME="src/post/veille-semaine-`date +%V-%Y`.md"
 BRANCH_NAME="veille_`date +%V-%Y`"
 COMMIT_MSG="Veille `date +%V-%Y`"
@@ -9,7 +8,7 @@ COMMIT_MSG="Veille `date +%V-%Y`"
 git checkout master
 git pull
 git checkout -b $BRANCH_NAME
-node ./bin/generate-post-veille.js --from-date "$LAST_DATE" > "$POST_FILENAME"
+node ./bin/generate-post-veille.js --from-date `cat $LAST_DATE_FILE` > "$POST_FILENAME"
 date +%s > $LAST_DATE_FILE
 git add $LAST_DATE_FILE
 git add $POST_FILENAME
