3e2bbdf922b6aa4326df6d335e4c38a79fc0beef
Damien Pobel <damien@pobel.fr>
2022-01-30 16:55:05 +0100

    refactor(ci): switch to Github Actions

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,19 +1,22 @@
 #! /bin/sh
 
-SSH_CMD="ssh -i ssh/id_dsa -o 'StrictHostKeyChecking no'"
+echo $ENCRYPT_KEY|gpg --batch --yes --passphrase-fd 0 --output ssh/id_ecdsa --decrypt ssh/id_ecdsa.enc
+chmod 600 ssh/id_ecdsa
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
+SSH_CMD="ssh -i ssh/id_ecdsa -o 'StrictHostKeyChecking no'"
+if [ "$GITHUB_EVENT_NAME" != "pull_request" ] ; then
     echo "## Pushing main site"
     rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/web/
     # trigger refresh of github profile README
-    curl -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -X POST -d "{\"ref\": \"main\"}" "https://api.github.com/repos/dpobel/dpobel/actions/workflows/main.yml/dispatches"
+    curl -H "Authorization: token $PERSONAL_TOKEN" -H "Accept: application/vnd.github.v3+json" -X POST -d "{\"ref\": \"main\"}" "https://api.github.com/repos/dpobel/dpobel/actions/workflows/main.yml/dispatches"
     # trigger refresh of my github page
-    curl -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -X POST -d "{\"ref\": \"main\"}" "https://api.github.com/repos/dpobel/dpobel.github.io/actions/workflows/main.yml/dispatches"
+    curl -H "Authorization: token $PERSONAL_TOKEN" -H "Accept: application/vnd.github.v3+json" -X POST -d "{\"ref\": \"main\"}" "https://api.github.com/repos/dpobel/dpobel.github.io/actions/workflows/main.yml/dispatches"
 else
-    echo "## Pushing to ${TRAVIS_PULL_REQUEST}.damien.pobel.fr"
-    rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/testing/${TRAVIS_PULL_REQUEST}.damien.pobel.fr
+    PR_ID=`echo $GITHUB_REF_NAME|cut -d '/' -f 1`
+    echo "## Pushing to ${PR_ID}.damien.pobel.fr"
+    rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/testing/${PR_ID}.damien.pobel.fr
     COMPARE=`./bin/compare.sh`
-    COMMENT=`echo "Test it at https://${TRAVIS_PULL_REQUEST}.damien.pobel.fr/\n\n---\n\n$COMPARE"`
+    COMMENT=`echo "Test it at https://${PR_ID}.damien.pobel.fr/\n\n---\n\n$COMPARE"`
     PAYLOAD=`jq -n --arg BODY "$COMMENT" '{body: $BODY}'`
-    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "$PAYLOAD" https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+    curl -H "Authorization: token $PERSONAL_TOKEN" -X POST -d "$PAYLOAD" https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_ID}/comments
 fi
