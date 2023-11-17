c7792f7879875b1281bbd1892de7dbf06917cd8c
Damien Pobel <damien@pobel.fr>
2023-11-17 22:53:26 +0100

    fix(deploy): adapt deploy to new server

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ chmod 600 ssh/id_ecdsa
 SSH_CMD="ssh -i ssh/id_ecdsa -o 'StrictHostKeyChecking no'"
 if [ "$GITHUB_EVENT_NAME" != "pull_request" ] ; then
     echo "## Pushing main site"
-    rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/web/
+    rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:prod/
     # trigger refresh of github profile README
     curl -H "Authorization: token $PERSONAL_TOKEN" -H "Accept: application/vnd.github.v3+json" -X POST -d "{\"ref\": \"main\"}" "https://api.github.com/repos/dpobel/dpobel/actions/workflows/main.yml/dispatches"
     # trigger refresh of my github page
@@ -14,7 +14,7 @@ if [ "$GITHUB_EVENT_NAME" != "pull_request" ] ; then
 else
     PR_ID=`echo $GITHUB_REF_NAME|cut -d '/' -f 1`
     echo "## Pushing to ${PR_ID}.damien.pobel.fr"
-    rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:~/testing/${PR_ID}.damien.pobel.fr
+    rsync -avcz --delete -e "$SSH_CMD" web/ dp@damien.pobel.fr:testing/${PR_ID}.damien.pobel.fr
     COMPARE=`./bin/compare.sh`
     COMMENT=`echo "Test it at https://${PR_ID}.damien.pobel.fr/\n\n---\n\n$COMPARE"`
     PAYLOAD=`jq -n --arg BODY "$COMMENT" '{body: $BODY}'`
